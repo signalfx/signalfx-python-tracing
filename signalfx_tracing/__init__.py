95fb0b170be876cfa12403603e4b34b63530ed0d
Owais Lone <owais@lone.pw>
2020-08-20 15:42:55 +0530

    Release v1.1.0
    New Features:
    
    - Added instrumentation for Falcon 2.0+

--- signalfx_tracing/__init__.py
+++ signalfx_tracing/__init__.py
@@ -3,7 +3,7 @@ from . import patch_span  # noqa
 from .instrumentation import instrument, uninstrument, auto_instrument  # noqa
 from .utils import create_tracer, trace  # noqa
 
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 
 # Django
 default_app_config = 'signalfx_tracing.libraries.django_.apps.SignalFxConfig'  # noqa
